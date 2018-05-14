fc9a80777fba1f849341dde5c0a95575716b5644
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-14 14:11:52 +0200

    Chain controller interface now catches error when malformed input

--- bp_orchestrator/chain_controller_interface.py
+++ bp_orchestrator/chain_controller_interface.py
@@ -2,7 +2,9 @@
 import json
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from .meta_scenario import SETTINGS_SYNC
+from .project_logger import set_up_logging
 
+LOG = set_up_logging(__name__)
 
 # pylint: disable= too-few-public-methods
 class ControllerInterface(WebSocket):
@@ -10,7 +12,13 @@ class ControllerInterface(WebSocket):
     # pylint: disable=invalid-name
     def handleMessage(self):
         print(self.data)
-        SETTINGS_SYNC.put(json.loads(self.data))
+        try:
+            json_data = json.loads(self.data)
+            SETTINGS_SYNC.put(json_data)
+        except Exception as error:
+            LOG.error('Error when handling incoming message from controller: %s', error)
+
+
 
 
 def start_controller_server(port: int):
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bp_orchestrator',
-      version='0.5',
+      version='0.6',
       description='Orchestrate blockchain scenarios',
       author='Anton von Weltzien',
       license='MIT',
