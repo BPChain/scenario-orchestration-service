5d0b5332bd1e84124467a18443f0c4ec618890ec
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-24 23:31:46 +0200

    Add tag. Please specify v0.8 in docker files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bp_orchestrator',
-      version='0.7',
+      version='0.8',
       description='Orchestrate blockchain scenarios',
       author='Anton von Weltzien',
       license='MIT',
